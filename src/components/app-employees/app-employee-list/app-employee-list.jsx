8fd7b4d1ca3c73c6e615726ff1b8cd033536e4b1
hayrapetyansami <samihayrapetyan@gmail.com>
2024-12-17 22:55:03 +0400

    added key prop in list

--- src/components/app-employees/app-employee-list/app-employee-list.jsx
+++ src/components/app-employees/app-employee-list/app-employee-list.jsx
@@ -2,11 +2,7 @@ import AppEmployeeItem from "../app-employee-item/app-employee-item";
 
 export default function AppEmployeeList({ data }) {
   const employees = data.map(item => {
-    // return <AppEmployeeItem
-    //   name={item.name}
-    //   salary={item.salary}
-    // />
-    return <AppEmployeeItem {...item} />;
+    return <AppEmployeeItem key={item.id} {...item} />;
   });
 
   return (
